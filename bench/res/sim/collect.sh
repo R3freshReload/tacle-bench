c997415eaefe621a33cc7e5a10484f0e79c411d3
Daniel Seifert <11018429+R3freshReload@users.noreply.github.com>
2024-11-22 11:04:46 +0100

    Fix xarg command

--- bench/res/sim/collect.sh
+++ bench/res/sim/collect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf ./arrayOpt/*
-find ../.. -type f | grep -e "arrayOpt_[a-zA-Z0-9]*.txt$" | gxargs -i cp {} arrayOpt
+find ../.. -type f | grep -e "arrayOpt_[a-zA-Z0-9]*.txt$" | xargs -i cp {} arrayOpt
 
 rm -rf ./opt/*
-find ../.. -type f | grep -e "opt_[a-zA-Z0-9]*.txt$" | gxargs -i cp {} opt
+find ../.. -type f | grep -e "opt_[a-zA-Z0-9]*.txt$" | xargs -i cp {} opt
 
 rm -rf ./noOpt/*
-find ../.. -type f | grep -e "noOpt_[a-zA-Z0-9]*.txt$" | gxargs -i cp {} noOpt
+find ../.. -type f | grep -e "noOpt_[a-zA-Z0-9]*.txt$" | xargs -i cp {} noOpt
